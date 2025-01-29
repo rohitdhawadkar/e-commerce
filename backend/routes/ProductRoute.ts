cceb8877930892c3a53a2e34fb220bc4bf49294e
rohitdhawadkar <144599518+rohitdhawadkar@users.noreply.github.com>
2025-01-29 20:20:30 +0530

    Added Product Routes

--- backend/routes/ProductRoute.ts
+++ backend/routes/ProductRoute.ts
@@ -1,10 +1,34 @@
 import { Product } from "@prisma/client";
-import { CreateProduct } from "../Controller/ProductController";
+import {
+  CreateProduct,
+  getAllProducts,
+  getProductByID,
+  updateProductByID,
+  deleteProductByID,
+} from "../Controller/ProductController";
 import { Router } from "express";
 const router: Router = Router();
 
 router.post<{}, {}, Product>("/createProduct", (req, res, next) => {
-  CreateProduct(req, res).catch(next); // Handle errors
+  CreateProduct(req, res).catch(next);
+});
+
+router.get<{}, {}>("/getProducts", (req, res, next) => {
+  getAllProducts(req, res).catch(next);
+});
+router.get<{ id: string }, {}>("/getProducts/:id", (req, res, next) => {
+  getProductByID(req, res).catch(next);
+});
+
+router.put<{ id: string }, {}, Product>(
+  "/updateProduct/:id",
+  (req, res, next) => {
+    updateProductByID(req, res).catch(next);
+  },
+);
+
+router.delete<{ id: string }, {}>("/deleteProduct/:id", (req, res, next) => {
+  deleteProductByID(req, res).catch(next);
 });
 
 export default router;
